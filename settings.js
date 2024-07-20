9ea6b050441f3b6c2b8f8eb5991214d754950351
Yungtripxee <TafadzwaMugombe2@gmail.com>
2024-07-20 21:42:07 +0200

    Update settings.jsv

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
 global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'YUNGTEE //owner name
-global.botname = '🕷Tasa' //name of the bot
+global.ownername = 'YUNGTEE' //owner name
+global.botname = 'Tasa' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
